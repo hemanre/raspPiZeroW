2bd06865cacee01be9a7a88103fc9a4fa55bf18d
hemanre <hemasaccademics@gmail.com>
2024-05-05 17:27:55 +0200

    updated dht11.py

--- dht11.py
+++ dht11.py
@@ -1,18 +1,34 @@
 import Adafruit_DHT
 import time
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 sensor = Adafruit_DHT.DHT11
 
 #GPIO18
 gpio_dht11 = 18
 
-while True:
-    #store readings in tuple
-    humidity, temp = Adafruit_DHT.read_retry(sensor, gpio_dht11)
+def main():
+    humidity_latest = 0
+    temp_latest = 0
+    while True:
+        #store readings in tuple
+        humidity, temp = Adafruit_DHT.read_retry(sensor, gpio_dht11)
+        
+        if (humidity != humidity_latest) or (temp != temp_latest):
+            humidity_latest = humidity
+            temp_latest = temp
+            #print('Temp={0:0.1f}°C, Humidity={1:0.1f}%'.format(temp, humidity))
+            logging.info('Temp={0:0.1f}°C, Humidity={1:0.1f}%'.format(temp, humidity))
+        elif (humidity == humidity_latest) and (temp == temp_latest):
+            pass
+        else:
+            #print('Failed to get reading. Try again')
+            logging.error('Reading not updated. Try again')
     
-    if humidity is not None and temp is not None:
-        print('Temp={0:0.1f}°C, Humidity={1:0.1f}%'.format(temp, humidity))
-    else:
-        print('Failed to get reading. Try again')
-    
-    time.sleep(2)
+if __name__ == '__main__':
+    main()
